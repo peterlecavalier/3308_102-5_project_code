d75d9470c1fcdfa8633f72834481888e59a67215
connoreamon <connor@eamonfamily.com>
2020-04-23 16:42:50 -0600

    Added coins and score

---
+++ Assets/CoinCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollection : MonoBehaviour
+{
+    public Text score;
+    private int scoreValue = 0; //int holding score
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            collision.gameObject.SetActive(false);
+            scoreValue += 1;
+            SetScore();
+        }
+    }
+    void SetScore()
+    {
+        score.text = scoreValue.ToString();//might need to mess around with
+    }
+
+}
